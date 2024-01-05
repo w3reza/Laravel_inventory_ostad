8c45a1c9cbd39e6b7d38cbf8eda656dca04637e1
w3reza <w3reza.ju@gmail.com>
2024-01-05 17:18:28 +0600

    pos-1

---
+++ pos/resources/views/backend/components/auth/login_form.blade.php
@@ -0,0 +1,54 @@
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-7 animated fadeIn col-lg-6 center-screen">
+            <div class="card w-90  p-4">
+                <div class="card-body">
+                    <h4>SIGN IN</h4>
+                    <br/>
+                    <input id="email" placeholder="User Email" class="form-control" type="email"/>
+                    <br/>
+                    <input id="password" placeholder="User Password" class="form-control" type="password"/>
+                    <br/>
+                    <button onclick="SubmitLogin()" class="btn w-100 bg-gradient-primary">Next</button>
+                    <hr/>
+                    <div class="float-end mt-3">
+                        <span>
+                            <a class="text-center ms-3 h6" href="{{url('/userRegistration')}}">Sign Up </a>
+                            <span class="ms-1">|</span>
+                            <a class="text-center ms-3 h6" href="{{url('/sendOtp')}}">Forget Password</a>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<script>
+    async function SubmitLogin() {
+        try {
+            let postBody = {
+                email: document.getElementById('email').value,
+                password: document.getElementById('password').value,
+            };
+
+            showLoader();
+            let response = await axios.post('/user_login', postBody);
+            hideLoader();
+            if(response.status===200 && response.data['status']==='success'){
+                setToken(response.data['token']);
+                successToast(response.data.message);
+                setTimeout(() => {
+                    window.location.href = "/userProfile";
+                }, 1000);
+            } else {
+                errorToast(response.data.message);
+            }
+        } catch (e) {
+            hideLoader();
+            errorToast('Something went wrong!');
+        }
+    }
+
+</script>
