2d094cabc8a834ac636c9efd142484ddbf49ea8d
w3reza <w3reza.ju@gmail.com>
2024-01-22 18:29:16 +0600

    invoice

--- pos_local/app/Models/Invoice.php
+++ pos_local/app/Models/Invoice.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Invoice extends Model
 {
     use HasFactory;
+    protected $filaable = [
+        'user_id',
+        'customer_id',
+        'total',
+        'discount',
+        'vat',
+        'payable'
+
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+
 }
